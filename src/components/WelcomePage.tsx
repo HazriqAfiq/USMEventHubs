8d7f538030a65a4a3d86f8b61483adcc930336bc
Muhammad Hazriq Afiq bin Saharudin <hazriqafiq22@gmail.com>
2025-12-19 15:34:59 +0000

    why on mobile view, i cannot click this (_for element <Button>_)

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -99,7 +99,7 @@ export function WelcomePage({ onGetStarted, videoSrc = '/videos/welcome-bg.mp4' 
             </div>
 
             {/* Bottom gradient fade */}
-            <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-black/80 to-transparent" />
+            <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-black/80 to-transparent z-0" />
         </div>
     );
 }
